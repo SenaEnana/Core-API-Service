fccc275d1a723252c5139fc92b40a518959c9c3d
SenaEnana <senaad47@gmail.com>
2026-09-25 16:09:27 +0300

    updated read single item method

--- app/main.py
+++ app/main.py
@@ -65,6 +65,19 @@ def create_item(item: ItemCreate, db: Session = Depends(get_db)):
 def read_items(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return db.query(ItemModel).offset(skip).limit(limit).all()
 
+@app.get(
+    f"{settings.API_PREFIX}/items/{{item_id}}",
+    response_model=ItemResponse,
+    tags=["Item"],
+)
+def read_item(item_id: int, db: Session = Depends(get_db)):
+    db_item= (
+        db.query(ItemModel).filter(ItemModel.id == item_id).first()
+    )
+    if db_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return db_item
+
 
 # @app.get("/health", tags=["API Health Check"])
 # async def health_check():
