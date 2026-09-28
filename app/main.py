6105273fd3de6daec9a1862f3b409186f6e5817a
SenaEnana <senaad47@gmail.com>
2026-09-28 10:00:00 +0300

    added one model for check up

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.config import settings
+from pydantic import BaseModel
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
     version=settings.VERSION,
     openapi_url=f"{settings.API_PREFIX}/openapi.json",
 )
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool | None = None
 
 app.add_middleware(
     CORSMiddleware,
@@ -19,13 +25,27 @@ app.add_middleware(
 def read_root():
     return {"Welcome to the Core API Service"}
 
-@app.get("/health", tags=["Health Check"])
+@app.get("/health", tags=["API Health Check"])
 async def health_check():
     return {
         "status": "healthy",
         "service": settings.PROJECT_NAME,
         "version": settings.VERSION,
     }
+
+
+
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: str | None = None):
+#     return {"item_id": item_id, "q": q}
+
+
+# @app.put("/items/{item_id}")
+# def update_item(item_id: int, item: Item):
+#     return {"item_name": item.name, "item_id": item_id}
+
 
 @app.get(f"{settings.API_PREFIX}/ping", tags=["Health Check"])
 async def ping():
